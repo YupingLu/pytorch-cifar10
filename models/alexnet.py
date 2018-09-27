96d59f88b770dd01bf77b08c0e0bb7ba6fbfad6a
YupingLu <yupinglu89@gmail.com>
2018-09-27 00:17:07 -0400

    update alexnet

--- models/alexnet.py
+++ models/alexnet.py
@@ -7,13 +7,13 @@ __all__ = ['AlexNet','alexnet']
 
 class AlexNet(nn.Module):
 
-    def __init__(self, num_classes=1000):
+    def __init__(self, num_classes=10):
         super(AlexNet, self).__init__()
         self.features = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=5, stride=2, padding=2),
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=5),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(64, 192, kernel_size=3, padding=1),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
             nn.Conv2d(192, 384, kernel_size=3, padding=1),
@@ -24,19 +24,11 @@ class AlexNet(nn.Module):
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
         )
-        self.classifier = nn.Sequential(
-            nn.Dropout(),
-            nn.Linear(256 * 2 * 2, 512),
-            nn.ReLU(inplace=True),
-            nn.Dropout(),
-            nn.Linear(512, 512),
-            nn.ReLU(inplace=True),
-            nn.Linear(512, num_classes),
-        )
+        self.classifier = nn.Linear(256, num_classes)
 
     def forward(self, x):
         x = self.features(x)
-        x = x.view(x.size(0), 256 * 2 * 2)
+        x = x.view(x.size(0), -1)
         x = self.classifier(x)
         return x
 
